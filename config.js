90167e30193bda180668d3a330ed220724b21822
lakaofc <lakaofc@gmail.com>
2024-12-03 01:59:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "youre session id",
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "ᴍᴀʟᴀᴋᴀ-ᴍᴅ=u3oHxAgY#BhLLA1w3LFZjgfFxtOVByusEWkDQpMk3vr4gxHcnTcA",
+MONGODB: process.env.MONGODB || "mongodb://mongo:CBBGzbzcwFbozEuPjhWkoXmzJRZxdsMz@autorack.proxy.rlwy.net:46461",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/QNwLWTN/20241201-230018.jpg",
-BOT_NAME: process.env.BOT_NAME || "➺ᴅᴀʀᴋ-ᴀʟꜰʜᴀ-ʙᴏᴛ࿐",
+BOT_NAME: process.env.BOT_NAME || "LAKA BOT",
 LANG: process.env.BOT_LANG || 'EN' ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 };
