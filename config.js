bd3004ef74b6a6bd865e009e91ac8acada34e2b1
lakaofc <lakaofc@gmail.com>
2024-12-04 23:53:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ᴍᴀʟᴀᴋᴀ-ᴍᴅ=u3oHxAgY#BhLLA1w3LFZjgfFxtOVByusEWkDQpMk3vr4gxHcnTcA",
-MONGODB: process.env.MONGODB || "mongodb://mongo:EdmkDTsYKtCkHvRZZYXVAkxmENsnjXbk@junction.proxy.rlwy.net:59101",
+MONGODB: process.env.MONGODB || "mongodb://mongo:cHFiHYhpIseEzFASKPHztzSRXPXKHFXO@junction.proxy.rlwy.net:43040",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/8DKVHKN/989f5b3ba02c6ae4a494537c8dad9cdb.jpg",
 BOT_NAME: process.env.BOT_NAME || "LAKA BOT",
 LANG: process.env.BOT_LANG || 'EN' ,
